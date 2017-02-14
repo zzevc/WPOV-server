b1005ebdbbd9f11249645bae19dcc586abd23329
Yesterday17 <t@yesterday17.cn>
2017-02-15 04:57:44 +0800

    更改bilibiliAPI的js名称 加入mysql支持（部分） 写作业去x

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
-
-var biliAPI = require('./bilibili/biliAPI.js')
+var bilibili = require('./bilibili/bilibili');
+var async = require('async');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -9,24 +9,68 @@ var connection = mysql.createConnection({
     database: 'WPOV'
 });
 
+var commands = {
+    songListExist: "SELECT COUNT(*) FROM information_schema.`TABLES` WHERE TABLE_NAME='songList';",
+    createSongList: "CREATE TABLE `songList` (aid varchar(20) NOT NULL)",
+    createSongTable: "CREATE TABLE `"
+}
+
 connection.connect();
 
-/*
-connection.query('CREATE TABLE ', function (error, results, fields){
-    //err
-})
-*/
+async.waterfall([
+    function (callback) {
+        connection.query(commands.songListExist, function (err, results, fields) {
+            callback(err, results[0]["COUNT(*)"]);
+        });
+    },
+    function (result, callback) {
+        if(result == 0){
+            connection.query(commands.createSongList, function(err, results, fields){
+                callback(err);
+            })
+        }
+    }
+], function (err, result) {
+    console.error(err);
+});
+
+bilibili.getAllVideoData(1, function(result){
+    /**
+     * 判断是否为VOCALOID中文曲有两种形式
+     * 1.主动判断法（优先级：1）
+     *      当一首歌曲中主动标有关键字[洛天依/乐正绫/言和/星尘/心华/乐正龙牙/南北组/……]时，
+     *   则判断该歌曲为VOCALOID中文曲。
+     * 
+     * 2.主动否决法（优先级：2）
+     *      当一首歌曲中标有关键字[UTAU/初音/双子/GUMI/……]市，则判断该歌曲非VOCALOID中文曲。
+     */
+
+    //咳咳……以上都是脑洞……周刊官方说了，只有带上VOCALOID中文曲标签才算在统计范围之内……
+
+    if(result.title.substring(0, 15) == "周刊VOCALOID中文排行榜")return;
+
+    for(var i in result.tags){
+        if(result.tags[i] == "VOCALOID中文曲"){
+            async.waterfall([
+                function(callback){
+                    connection.query("SELECT COUNT(*) FROM information_schema.`TABLES` WHERE TABLE_NAME='av" + result.aid + "';", function(err, results, fields){
+                        callback(err, results[0]["COUNT(*)"]);
+                    });
+                },
+                function(exist, callback){
+                    if(exist){
+                        connection.query("INSERT INTO av" + result.aid + "()");
+                    }
+                    else{
+                        connection.query("CREATE TABLE");
+                    }
+                }
+            ])
+
+            //console.log(JSON.stringify(result, null, 4));
+            break;
+        }
+    }
+});
+
 connection.end();
-
-biliAPI.getAllVideoData(1);
-
-/**
- * Bilibili API
- * 
- * http://api.bilibili.com/x/v2/reply?jsonp=jsonp&pn=1&type=1&oid=10492&sort=0&_=1482889773903
- * http://api.bilibili.com/x/tag/archive/tags?callback=jQuery17202870352235622704_1482889079894&aid=1048576&type=jsonp&_=1482889080659
- * 
- * 获取所有视频数据
- * http://api.bilibili.com/archive_rank/getarchiverankbypartion?type=jsonp&tid=30&pn=1
- * 
- */
--- bilibili/bilibili.js
+++ bilibili/bilibili.js
@@ -1,6 +1,22 @@
+/**
+ * Bilibili API
+ * 
+ * http://api.bilibili.com/x/v2/reply?jsonp=jsonp&pn=1&type=1&oid=10492&sort=0&_=1482889773903
+ * http://api.bilibili.com/x/tag/archive/tags?callback=jQuery17202870352235622704_1482889079894&aid=1048576&type=jsonp&_=1482889080659
+ * 
+ * 获取所有视频数据
+ * http://api.bilibili.com/archive_rank/getarchiverankbypartion?type=jsonp&tid=30&pn=1
+ * 
+ */
+
 var async = require('async');
 var http = require('http');
 var fs = require('fs');
+
+var biliGetDate = function (c) {
+    var d = new Date();
+    return d.getFullYear() + c + d.getMonth() + c + d.getDate();
+}
 
 var getVideoNum = function (callback) {
     var url = "http://api.bilibili.com/archive_rank/getarchiverankbypartion?type=jsonp&tid=30&ps=1&pn=1";
@@ -20,7 +36,7 @@ var getVideoNum = function (callback) {
     });
 };
 
-var getAllVideoData = function (begin) {
+var getAllVideoData = function (begin, cbAfter) {
     getVideoNum(function (err, total) {
         var arr = [], result = [], fetch = 0, last = "", offset = 0, backcheck = false;
         var getVideoData = function (id, callback) {
@@ -48,7 +64,6 @@ var getAllVideoData = function (begin) {
                                     duration: data.duration,
                                     tags: data.tags,
                                     stat: data.stat,
-                                    play: data.play,
                                     create: data.create,
                                     description: data.description,
                                     mid: data.mid,
@@ -61,10 +76,14 @@ var getAllVideoData = function (begin) {
                                 arr.push(total);
                             }
                             else {
-                                if(backcheck) result.unshift(profile);
-                                else result.push(profile);
-
-                                console.log(id);
+                                //console.log(id);
+                                if (typeof cbAfter != "function") {
+                                    if (backcheck) result.unshift(profile);
+                                    else result.push(profile);
+                                }
+                                else {
+                                    cbAfter(profile);
+                                }
 
                                 if (id == total) callback(id);
                                 else callback(null);
@@ -94,8 +113,9 @@ var getAllVideoData = function (begin) {
                 console.log("Till: ", err);
                 console.log("Total(Offset): ", offset);
 
+                var d = new Date();
                 //Save to File.
-                fs.writeFile('results.json', JSON.stringify(result, null, 4), 'utf8', (err) => {
+                fs.writeFile("./results/total_" + biliGetDate(".") + ".json", JSON.stringify(result, null, 4), 'utf8', (err) => {
                     if (err) throw err;
                     console.log("Filewrite success!");
                 });
